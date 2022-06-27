36dc9a43a0e1a2649d24e62de69564a86dbd07f1
rjawesome <aonedtop@gmail.com>
2022-06-27 13:52:11 -0700

    handle multiple genes per pathway/disease pair

--- parser.py
+++ parser.py
@@ -35,28 +35,43 @@ def load_data(data_folder: str):
       pathway_categories[row_id] = []
     pathway_categories[row_id].append(row['CATEGORY_NAME'])
 
+  docs: dict[str, any] = {}
+
   #create json for pathway/disease
   for row in pathway_disease_info:
-    doc = {
-      'subject': {
-        'DISEASE_NAME': row['DISEASE_NAME'],
-        'PHENO_TYPE': -1 if row['PHENO_TYPE'] == '' else int(row['PHENO_TYPE'])
-      },
-      'object': {
-        'PATHWAY_ID': row['PATHWAY_ID'],
-        'PATHWAY_NAME': row['PATHWAY_NAME'],
-        'PATHWAY_CATEGORIES': pathway_categories[row['PATHWAY_ID']]
-      },
-      'relation': {
+    doc_id = f"{row['PATHWAY_ID']}-{row['DISEASE_NAME']}"
+
+    #just append gene if we have already processed pathway-disease pair
+    if doc_id in docs:
+      docs[doc_id]['relation'].append({
         'GENE_ID': int(row['GENE_ID']),
         'GENE_SYMBOL': row['GENE_SYMBOL'],
         'MIM_ID': int(row['MIM_ID'])
-      },
-      'predicate': 'related_to'
-    }
+      })
+    else:
+      docs[doc_id] = {
+        '_id': doc_id,
+        'subject': {
+          'DISEASE_NAME': row['DISEASE_NAME'],
+          'PHENO_TYPE': -1 if row['PHENO_TYPE'] == '' else int(row['PHENO_TYPE'])
+        },
+        'object': {
+          'PATHWAY_ID': row['PATHWAY_ID'],
+          'PATHWAY_NAME': row['PATHWAY_NAME'],
+          'PATHWAY_CATEGORIES': pathway_categories[row['PATHWAY_ID']]
+        },
+        'relation': [{
+          'GENE_ID': int(row['GENE_ID']),
+          'GENE_SYMBOL': row['GENE_SYMBOL'],
+          'MIM_ID': int(row['MIM_ID'])
+        }],
+        'predicate': 'related_to'
+      }
+  
+  for doc_id in docs:
+    yield docs[doc_id]
+    
 
-    yield doc
-
-# with open("./output.json", "w") as f:
-#   for i in load_data("./data"):
-#     f.write(str(i) + "\n")
+with open("./output.json", "w") as f:
+  for i in load_data("./data"):
+    f.write(str(i) + "\n")
